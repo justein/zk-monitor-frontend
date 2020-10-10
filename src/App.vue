97a6668f7366ee5d6476ba833896b08c202d5712
justein <james_gosling@163.com>
2020-10-10 18:52:49 +0800

    初始提交

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+ import axios from 'axios'
+ export default {
+        data: function () {
+            return {
+                loadData(pramas) {
+                    return axios.get("static/datatable.json",pramas).then(res =>{
+                        return res.data;
+                    });
+                },
+                columns:[
+                    {title:"歌名",field:'name'},
+                    {title:"时长",field:'time'},
+                    {title:"歌手",field:'singer'},
+                    {title:"专辑",field:'album'},
+                ]
+            }
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+      <v-alert type="success" message="Zookeeper管理工具"></v-alert>
+    <v-button type="primary">添加</v-button>
+    <v-button type="success">更新</v-button>
+    <v-button type="error">删除</v-button>
+
+    <v-data-table :data='loadData' :columns='columns' stripe bordered></v-data-table>
+  </div>
+  
+</template>
